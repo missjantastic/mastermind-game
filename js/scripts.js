0551a1a150c7473f6190fd922fb56eb2e8312d32
Janina <janinatalvarez@Gmail.com>
2021-11-18 00:53:27 -0800

    clean code and add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let guesses;
-let comboArray;
+let guessesLeft;
+let pattern;
 newGame();
 
 function httpGetAsync(theUrl, callback)
@@ -15,14 +15,17 @@ function httpGetAsync(theUrl, callback)
 
 function newGame(){
     guesses = 10;
+
+    //retrieve pattern of four random numbers from API and store them into an array
     httpGetAsync('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new', function(result){
-        comboArray = result.split("\n");
-        comboArray.pop();
-        console.log(comboArray);
+        pattern = result.split("\n");
+        pattern.pop();
+        console.log(pattern);
     });
 }
 
 function checkSubmit(e) {
+    //if player clicks enter key, register a submitted guess
     if(e && e.keyCode == 13) {
         onSubmit();
      }
@@ -30,38 +33,39 @@ function checkSubmit(e) {
 
 
 function onSubmit() {
+    //load player guesses into an array
     let slot1 = document.getElementById("slot1").value;
     let slot2 = document.getElementById("slot2").value;
     let slot3 = document.getElementById("slot3").value;
     let slot4 = document.getElementById("slot4").value;
-    let guessArray = [slot1, slot2, slot3, slot4];
+    let playerGuesses = [slot1, slot2, slot3, slot4];
 
-    let isValidated = validateGuesses(slot1, slot2, slot3, slot4);
+    //validate that input meets requiremets
+    let isValidated = validateInput(slot1, slot2, slot3, slot4);
     
+    //if the input is not valid, exit function/don't submit input
     if(!isValidated) {
         return;
     }
 
+    //if they run out of guesses, end game
     if(countGuesses()==0){
         alert("You lose! Game will now restart");
         location.reload();
     }
 
-    if(compareAnswers(guessArray))
-    {
-        alert("You won! Game will now restart");
-        location.reload();
-    }
+    submitGuess(playerGuesses);
 }
 
-function validateGuesses(slot1, slot2, slot3, slot4) {
+function validateInput(slot1, slot2, slot3, slot4) {
     let validator = document.getElementById("validator");
 
+    //if any of the input fiels are empty
     if(slot1 == "" || slot2 == "" || slot3 == "" || slot4 == "") {
         validator.innerHTML= "Make sure you have a guess for each slot!";
         validator.style.display="block";
         return false;
-    } else if(slot1 > 7 || slot1 < 0 || slot2 > 7 || slot2 < 0 || slot3 > 7 || slot3 < 0 || slot4 > 7 || slot4 < 0) {
+    } else if(slot1 > 7 || slot1 < 0 || slot2 > 7 || slot2 < 0 || slot3 > 7 || slot3 < 0 || slot4 > 7 || slot4 < 0) { //if any of the inputs are not between 0 and 7
         validator.innerHTML= "Make sure your guesses are between 0 and 7!";
         validator.style.display="block";
         return false;
@@ -72,57 +76,69 @@ function validateGuesses(slot1, slot2, slot3, slot4) {
 }
 
 function countGuesses(){
-    let guessesLeft = document.getElementById("guessesLeft");
-    guesses = guesses - 1;
-    guessesLeft.innerHTML= `You have ${guesses} guesses remaining!`;
-    if (guesses == 0) {
-        return 0;
-    }
+    let guessesLeftText = document.getElementById("guessesLeft");
+    guessesLeft -= 1;
+    guessesLeftText.innerHTML= `You have ${guesses} guesses remaining!`;
+    return guessesLeft;
 }
 
-function compareAnswers(guessArray){
+function submitGuess(playerGuesses){
     let allCorrect = false;
-    let CorrectLoc = 0;
-    let CorrectNum = 0;
+    let locMatches = 0;
+    let existingNums = 0;
+
+    //an array containing the differences of pattern - playerGuesses
+    //an index with zero would indicate the index of a location match
+    let differences = pattern.map((slot, i) => slot - playerGuesses[i]);
+
+    //an array of zeros, used to count the matches (sourced from differences)
+    let matches = differences.filter(match => match == 0);
+    
+    if (matches.length == 4){ //if all four guesses are correct
+        allCorrect = true;
+    } else {
+        locMatches = matches.length;
+
+        //this removes any location matches so they are no longer considered
+        for (let i = 0; i < differences.length; i++) {
+            if (differences[i] == 0){
+                pattern.splice(i, 1);
+            }
+        }
+        //this checks for matches that are not in the right location
+        for (let i = 0; i < playerGuesses.length; i++) {
+            if (pattern.includes(playerGuesses[i])){
+                existingNums += 1;
+            }
+        }
+    }
+
+    sendResponse(allCorrect, locMatches, existingNums);
+    return;
+}
+
+function sendResponse(allCorrect, locMatches, existingNums){
     let feedback = document.createElement("p");
     let message = "";
     let element = document.getElementById("feedback_log");
-    let compareArray = comboArray.map((slot, i) => slot - guessArray[i]);
-    console.log(compareArray);
 
-    let matches = compareArray.filter(match => match == 0);
-    console.log(matches);
-
-    
-    if (matches.length == 4){
-        allCorrect = true;
-        return allCorrect;
-    } else {
-
-        CorrectLoc = matches.length;
-        for (let i = 0; i < compareArray.length; i++) {
-            if (compareArray[i] == 0){
-                comboArray.splice(i, 1);
-            }
-        }
-        for (let i = 0; i < guessArray.length; i++) {
-            if (comboArray.includes(guessArray[i])){
-                CorrectNum += 1;
-            }
-        }
-    }
-
+    //run this code if the entire guess is correct
     if (allCorrect) {
-        message = "Wow! You guessed the combination! You're a master codebreaker!";
+        message = "Wow! You guessed the pattern! You're a master codebreaker! The game will restart now";
+        location.reload();
     }
-    else if (CorrectLoc > 0 || CorrectNum > 0){
-        message = `You have ${CorrectLoc} of your guesses in the correct location. You have also guessed ${CorrectNum} matching numbers that are in the wrong location. The rest are incorrect.`;
-    } else {
+    //else run this code if any part of their guess is correct in some way
+    else if (locMatches > 0 || existingNums > 0){
+        message = `You have ${locMatches} of your guesses in the correct location. You have also guessed ${existingNums} matching numbers that are in the wrong location. The rest are incorrect.`;
+    } 
+    //else run this code is their guess is completely incorrect
+    else {
         message = "None of these numbers are correct.";
     }
 
     let feedbackText = document.createTextNode(message);
     feedback.appendChild(feedbackText);
     element.appendChild(feedback);
-    return allCorrect;
+
+    return;
 }
